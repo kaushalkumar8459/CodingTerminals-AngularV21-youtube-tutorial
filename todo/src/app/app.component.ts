20814ec1e92955ca6086edebcd25216ccb488cd2
kaushalkumar8459 <kaushalkumar8459@gmail.com>
2026-01-10 15:32:28 +0530

    cleanup

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { AboutComponent } from './about/about.component';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HomeComponent,
-    GalleryComponent, AboutComponent
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo';
+
+  imageName = signal('stone');
+  avatarUrl = signal('https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg');
+
+
+  count = signal(1);
+
+  // with signal
+  doubleCount = signal(this.count());
+
+  // with computed
+  // doubleCount = computed(() => this.count() * 2);
+
+  increase() {
+    this.count.update(value => value + 1);
+  }
 }
+
