234d4c0e7deafbda16fbffa622f1a2691525550d
kaushalkumar8459 <kaushalkumar8459@gmail.com>
2026-01-28 07:22:59 +0530

    cleanup

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -10,21 +10,4 @@ import { Component, computed, signal } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
-
-  imageName = signal('stone');
-  avatarUrl = signal('https://static.desygner.com/wp-content/uploads/sites/13/2022/05/04141642/Free-Stock-Photos-01.jpg');
-
-
-  count = signal(1);
-
-  // with signal
-  doubleCount = signal(this.count());
-
-  // with computed
-  // doubleCount = computed(() => this.count() * 2);
-
-  increase() {
-    this.count.update(value => value + 1);
-  }
 }
-
